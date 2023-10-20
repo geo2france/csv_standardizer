9078c9c5248317c5616614044168687c665e072a
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2023-10-20 14:46:21 +0200

    __name__ = 'main'

--- standardize_csv.py
+++ standardize_csv.py
@@ -23,44 +23,47 @@ TODO : I/O fichiers
 TODO2 : Générer un csvt ?
 """
 
-def rename_duplicates(lst):
-    """
-    Pour renommer les entêtes identiques (col, col_1, col_2, etc.)
-    """
-    seen = {}
-    new_lst = []
-    for item in lst:
-        if item not in seen:
-            seen[item] = 1
-            new_lst.append(item)
-        else:
-            seen[item] += 1
-            new_lst.append(f"{item}_{seen[item] - 1}")
-    return new_lst
+def standardize():
+    def rename_duplicates(lst):
+        """
+        Pour renommer les entêtes identiques (col, col_1, col_2, etc.)
+        """
+        seen = {}
+        new_lst = []
+        for item in lst:
+            if item not in seen:
+                seen[item] = 1
+                new_lst.append(item)
+            else:
+                seen[item] += 1
+                new_lst.append(f"{item}_{seen[item] - 1}")
+        return new_lst
 
 
-input_data = stdin.buffer.read() # read as binary
-encoding_detection = chardet.detect(input_data)
-encoding, confidence = encoding_detection['encoding'], round(encoding_detection['confidence'],2)
+    input_data = stdin.buffer.read() # read as binary
+    encoding_detection = chardet.detect(input_data)
+    encoding, confidence = encoding_detection['encoding'], round(encoding_detection['confidence'],2)
 
-print('Encoding : {} (with {} confidence)'.format(encoding, confidence), file=stderr)
+    print('Encoding : {} (with {} confidence)'.format(encoding, confidence), file=stderr)
 
-csvfile = io.StringIO(input_data.decode(encoding))
-dialect = clevercsv.Sniffer().sniff(csvfile.read())
-print('Dialect : {}'.format(dialect.to_dict()), file=stderr)
+    csvfile = io.StringIO(input_data.decode(encoding))
+    dialect = clevercsv.Sniffer().sniff(csvfile.read())
+    print('Dialect : {}'.format(dialect.to_dict()), file=stderr)
 
-csvfile.seek(0)
-reader = clevercsv.reader(csvfile, dialect)
-rows = reader
+    csvfile.seek(0)
+    reader = clevercsv.reader(csvfile, dialect)
+    rows = reader
 
-## Write new file
-writer = clevercsv.write.writer(stdout, encoding='utf8') #Print to stdout
-header = True
-for row in rows :
-    if header :
-        row = rename_duplicates(row)
-    header = False
-    writer.writerow(row)
+    ## Write new file
+    writer = clevercsv.write.writer(stdout, encoding='utf8') #Print to stdout
+    header = True
+    for row in rows :
+        if header :
+            row = rename_duplicates(row)
+        header = False
+        writer.writerow(row)
 
+if __name__ == '__main__':
+    standardize()
 
 
